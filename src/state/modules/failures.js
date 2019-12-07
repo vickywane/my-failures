d341ec19eaade65545a1aed2e8b33dd8f09dfc8f
victory <vickywane@gmail.com>
2019-12-07 12:11:04 +0100

    ...

--- src/state/modules/failures.js
+++ src/state/modules/failures.js
@@ -1,24 +1,24 @@
 const state = {
 	failures: [
-	{	id: 1,
-		name: "Failed Jamb",
-		retries: 1
-	}  ,
+		{ id: 1, name: "Failed Jamb", retries: 1 },
 
-	{	id: 2,
-		name: "Failed Jamb",
-		retries: 2
-	}  ,  
-] 
-}
+		{ id: 2, name: "Failed Jamb", retries: 2 }
+	]
+};
 
 const getters = {
 	allFailures: state => state.failures
 };
 
-const actions = {};
+const actions = {
+	addFailure({ commit }, name) {
+		commit("newFailure");
+	}
+};
 
-const mutations = {};
+const mutations = {
+	newFailure: (state.failures) => state.failures.pop()
+};
 
 export default {
 	state,
